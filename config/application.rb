c9f19a96ba77cdeb49d8ebd11560f77e2357235a
Yo-Su <57243991+Yo-Su@users.noreply.github.com>
2019-11-19 14:27:52 +0900

    update config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -19,15 +19,11 @@ Bundler.require(*Rails.groups)
 
 module PcPartslistSite
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-
-    # Don't generate system test files.
-    config.generators.system_tests = nil
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.test_framework false
+    end
   end
 end
