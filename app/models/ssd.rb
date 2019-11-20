9cb2fbcb2c6d50a00f010ba88acd7cd28ac640ef
Yo-Su <57243991+Yo-Su@users.noreply.github.com>
2019-11-20 21:04:14 +0900

    add model SSD

---
+++ app/models/ssd.rb
@@ -0,0 +1,2 @@
+class Ssd < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_20_115320) do
+ActiveRecord::Schema.define(version: 2019_11_20_120244) do
 
   create_table "cpus", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -75,6 +75,17 @@ ActiveRecord::Schema.define(version: 2019_11_20_115320) do
 
   create_table "pcparts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "category"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "ssds", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "brand"
+    t.string "capacity"
+    t.string "size"
+    t.string "interface"
+    t.string "type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
