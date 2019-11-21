779defa326ebf3ec618ac647685889583ae3bb10
Yo-Su <57243991+Yo-Su@users.noreply.github.com>
2019-11-21 19:58:13 +0900

    add cpu_db item_value_column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_21_084347) do
+ActiveRecord::Schema.define(version: 2019_11_21_105553) do
 
   create_table "cpucoolers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2019_11_21_084347) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "pcpart_id"
+    t.string "item_value"
   end
 
   create_table "displays", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
