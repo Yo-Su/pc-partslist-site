98f4b41215853763cc24f585244b80d1fd251edd
Yo-Su <57243991+Yo-Su@users.noreply.github.com>
2019-11-21 16:10:08 +0900

    update pcparts_controller remove create_action

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   root to:  'parts_lists#index'
   get '/kakaku', to: 'kakakus#index'
   devise_for :users
-  resources :pcparts, only: [:index, :show, :create] do
-    resources :cpus, only: :index
+  resources :pcparts, only: [:index, :show] do
+    resources :cpus, only: [:index, :create]
     resources :mbs, only: :index
   end
 end
