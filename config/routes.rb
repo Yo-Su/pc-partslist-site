2f0e0c715ccfc41358d1efec944324ca63b16e81
Yo-Su <57243991+Yo-Su@users.noreply.github.com>
2019-11-21 14:12:03 +0900

    add kakakus_controller (test get_cpu_data)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to:  'parts_lists#index'
+  get '/kakaku', to: 'kakakus#index'
   devise_for :users
   resources :pcparts, only: [:index, :show] do
     resources :cpus, only: :index
