8fc3f5a8684067daa455642580cd4b5a4b3126b0
Yo-Su <57243991+Yo-Su@users.noreply.github.com>
2019-11-22 17:19:54 +0900

    add new action

--- app/controllers/parts_lists_controller.rb
+++ app/controllers/parts_lists_controller.rb
@@ -4,4 +4,15 @@ class PartsListsController < ApplicationController
     @lists = @user.parts_lists.includes(:user)
   end
 
+  def new
+    @parts_list = PartsList.new(name: "testlist", public_private: 1, user_id: current_user.id)
+    @parts_list.save
+    redirect_to user_parts_lists_path(current_user.id)
+  end
+
+  private
+    def parts_list_params
+      params.merge(name: "testlist", public_private: 1).merge(user_id: current_user.id)
+    end
+
 end
