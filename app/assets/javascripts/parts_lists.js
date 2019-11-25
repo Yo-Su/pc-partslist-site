f44fa880ff6fbaedf4b99213072a3580414cd532
Yo-Su <57243991+Yo-Su@users.noreply.github.com>
2019-11-25 17:10:14 +0900

    update parts_list update motherboard

--- app/assets/javascripts/parts_lists.js
+++ app/assets/javascripts/parts_lists.js
@@ -2,9 +2,15 @@ $(function(){
   // matchメソッドでurlからカテゴリーIDを取得する（パーツカテゴリーページのみ有効）
   var category_id = ""
   const category = location.href.match(/pcparts\/(\d+)/) ;
-  if (category && category[1] == 1){
-    category_id = "cpu_id"
+  if (category){
+    switch (category[1].toString()){
+      case "1" : category_id = "cpu_id"; break;
+      case "2" : category_id = "mb_id"; break;
+    }
   }
+  // if (category && category[1] == 1){
+  //   category_id = "cpu_id"
+  // }
 
   // フォームにtype="hidden"のinput要素を追加
   $('form').append(`<input name=${category_id} type="hidden" value="" />`);
