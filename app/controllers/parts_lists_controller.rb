effdb17a38935ba7851b71d8c141edd7d7b3ca44
Yo-Su <57243991+Yo-Su@users.noreply.github.com>
2019-11-27 17:08:25 +0900

    add before_action signed_in_user, only_current_user ログインしていない場合の処理を追加 自分以外のパーツリストを見られないようにした

--- app/controllers/parts_lists_controller.rb
+++ app/controllers/parts_lists_controller.rb
@@ -1,4 +1,7 @@
 class PartsListsController < ApplicationController
+  before_action :signed_in_user
+  before_action :only_current_user
+
   def index
     @user = User.find(params[:user_id])
     @lists = @user.parts_lists.includes(:user)
@@ -40,6 +43,18 @@ class PartsListsController < ApplicationController
   private
     def parts_list_params
       params.permit(:name, :cpu_id, :mb_id, :memory_id, :hdd_id, :ssd_id, :videocard_id, :power_id, :pccase_id, :cpucooler_id, :display_id).merge(public_private: 1).merge(user_id: current_user.id)
+    end
+
+    def signed_in_user
+      unless signed_in?
+        redirect_to root_path
+        flash[:alert] = "ログインして下さい"
+      end
+    end
+
+    def only_current_user
+      @user = User.find(params[:user_id])
+      redirect_to user_parts_lists_path(current_user.id) unless current_user == @user
     end
 
 end
