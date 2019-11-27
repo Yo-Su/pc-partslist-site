eb8eb94bd2920da7f00bd322d0b6d1718df2d7ed
Yo-Su <57243991+Yo-Su@users.noreply.github.com>
2019-11-27 21:33:30 +0900

    fixed parts_lists.js function if category_id category_idがある場合のみjsが動くように変更

--- app/assets/javascripts/parts_lists.js
+++ app/assets/javascripts/parts_lists.js
@@ -16,16 +16,15 @@ $(function(){
       case "10" : category_id = "display_id"; break;
     }
   }
-  // if (category && category[1] == 1){
-  //   category_id = "cpu_id"
-  // }
 
-  // フォームにtype="hidden"のinput要素を追加
-  $('form').append(`<input name=${category_id} type="hidden" value="" />`);
+  if (category_id){
+    // フォームにtype="hidden"のinput要素を追加
+    $('form').append(`<input name=${category_id} type="hidden" value="" />`);
 
-  // 各ボタンにはname属性にidを付与している
-  // ボタンクリック時、type="hidden"のinput要素に自分の値を設定
-  $('input').click(function(){
-      $(`input[name=${category_id}]`).val($(this).attr('name'));
-    });
+    // 各ボタンにはname属性にidを付与している
+    // ボタンクリック時、type="hidden"のinput要素に自分の値を設定
+    $('input').click(function(){
+        $(`input[name=${category_id}]`).val($(this).attr('name'));
+      });
+  }
 });
