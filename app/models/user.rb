9ab79286845d5f24bfa626e198b2908d90a19ef6
Yo-Su <57243991+Yo-Su@users.noreply.github.com>
2020-01-06 10:09:10 +0900

    add ユーザー関連のバリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :parts_lists
+
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 7 }
+  validates :name, presence: true, uniqueness: true, length: { minimum: 4 }
 end
